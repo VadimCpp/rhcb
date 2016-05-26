c6dba031201b6d082043180e5209ae58250fe812
vadim.kaninskiy <vadim.kaninskiy@iksatprof.ru>
2016-05-26 14:49:56 +0200

    Add sass styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('copy-html', function() {
   return gulp.src('src/index.html')
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', [ 'copy-html' ]);
+gulp.task('compile-sass', function() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', [ 'copy-html', 'compile-sass' ]);
 
 gulp.task('default', [ 'build' ]);
