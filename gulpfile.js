249aad7b248b7c50da0a844256a281a4efb38ea7
vadim.kaninskiy <vadim.kaninskiy@iksatprof.ru>
2016-05-26 16:49:36 +0200

    Complete one recipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var spritesmith = require('gulp.spritesmith');
 
 gulp.task('copy-html', function() {
   return gulp.src('src/index.html')
@@ -10,6 +11,18 @@ gulp.task('compile-sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sprite', function() {
+  var spriteData =
+    gulp.src('./src/img/materials/*.png')
+      .pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css',
+      }));
+
+  spriteData.img.pipe(gulp.dest('./dist/img/')); // путь, куда сохраняем картинку
+  spriteData.css.pipe(gulp.dest('./dist/css/')); // путь, куда сохраняем стили
 });
 
 gulp.task('build', [ 'copy-html', 'compile-sass' ]);
